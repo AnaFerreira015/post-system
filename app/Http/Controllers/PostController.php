25053d44d3d2dcbfe514b5211a74e5dbb3ca2314
anaferreira015 <dasilvaferreira.af@hotmail.com>
2020-01-06 14:03:37 -0300

    Handles request to return post as PostResource

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -36,7 +36,30 @@ class PostController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $this->validate($request, [
+            'title' => 'required',
+            'body' => 'required',
+            'user_id' => 'required',            
+            'image' => 'required|mimes:jpeg,png,jpg,gif,svg',
+        ]);
+
+        $post = new Post;
+
+        if ($request->hasFile('image')) {
+            $image = $request->file('image');
+            $name = str_slug($request->title).'.'.$image->getClientOriginalExtension();
+            $destinationPath = public_path('/uploads/posts');
+            $imagePath = $destinationPath . "/" . $name;
+            $image->move($destinationPath, $name);
+            $post->image = $name;
+        }
+
+        $post->user_id = $request->user_id;
+        $post->title = $request->title;
+        $post->body = $request->body;
+        $post->save();
+
+        return new PostResource($post);
     }
 
     /**
