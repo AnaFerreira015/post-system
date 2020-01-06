2cbdc8bb5aac2183910ea85a8b573e5c7762e169
anaferreira015 <dasilvaferreira.af@hotmail.com>
2020-01-06 10:13:05 -0300

    Initial commit

---
+++ database/seeds/UserTableSeeder.php
@@ -0,0 +1,27 @@
+<?php 
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+use App\User;
+use App\Role;
+
+class UserTableSeeder extends Seeder
+{
+
+    public function run()
+    {
+        $user = new User;
+        $user->name = 'Samuel Jackson';
+        $user->email = 'samueljackson@jackson.com';
+        $user->password = bcrypt('samuel1234');
+        $user->save();
+        $user->roles()->attach(Role::where('name', 'user')->first());
+
+        $admin = new User;
+        $admin->name = 'Neo Ighodaro';
+        $admin->email = 'neo@creativitykills.co';
+        $admin->password = bcrypt('neo1234');
+        $admin->save();
+        $admin->roles()->attach(Role::where('name', 'admin')->first());
+    }
+}
