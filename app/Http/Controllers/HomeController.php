aaeecdaebc64855e20ac8ff8183d46c892f3628b
anaferreira015 <dasilvaferreira.af@hotmail.com>
2020-01-07 17:19:10 -0300

    Update redirect

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -23,7 +23,7 @@ class HomeController extends Controller
      */
     public function index(Request $request)
     {
-        if ($request->user()->hasRole('user')) return redirect('/');
+        if ($request->user()->hasRole('user')) return view('home');
 
         if ($request->user()->hasRole('admin')) return redirect('/admin/dashboard');
     }
