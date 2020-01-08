a0cb84525858edd81edd84c82556b1c527a1f9f1
anaferreira015 <dasilvaferreira.af@hotmail.com>
2020-01-08 19:14:00 -0300

    Build model update

---
+++ resources/js/components/Update.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="container">
+    <form>
+        <div :class="['form-group m-1 p-3', successful ? 'alert-success' : '']">
+        <span v-if="successful" class="label label-sucess">Updated!</span>
+        </div>
+
+        <div :class="['form-group m-1 p-3', error ? 'alert-danger' : '']">
+        <span v-if="errors.title" class="label label-danger">
+            {{ errors.title[0] }}
+        </span>
+        <span v-if="errors.body" class="label label-danger">
+            {{ errors.body[0] }}
+        </span>
+        </div>
+
+        <div class="form-group">
+        <input type="title" ref="title" class="form-control" id="title" placeholder="Enter title" required>
+        </div>
+
+        <div class="form-group">
+        <textarea class="form-control" ref="body" id="body" placeholder="Enter a body" rows="8" required></textarea>
+        </div>
+
+        <button type="submit" @click.prevent="update" class="btn btn-primary block">
+        Submit
+        </button>
+    </form>
+    </div>
+</template>
