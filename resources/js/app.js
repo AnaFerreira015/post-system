b97c1b26b2460c191001562b63df148ad339cc46
anaferreira015 <dasilvaferreira.af@hotmail.com>
2020-01-08 19:06:35 -0300

    Includes config of /create and /update routes

--- resources/js/app.js
+++ resources/js/app.js
@@ -3,7 +3,9 @@ require('./bootstrap');
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Homepage from './components/Homepage'
+import Create from './components/Create'
 import Read from './components/Read'
+import Update from './components/Update'
 
 Vue.use(VueRouter)
 
@@ -14,6 +16,18 @@ const router = new VueRouter({
             path: '/admin/dashboard',
             name: 'read',
             component: Read,
+            props: true
+        },
+        {
+            path: '/admin/create',
+            name: 'create',
+            component: Create,
+            props: true
+        },
+        {
+            path: '/admin/update',
+            name: 'update',
+            component: Update,
             props: true
         },
     ],
