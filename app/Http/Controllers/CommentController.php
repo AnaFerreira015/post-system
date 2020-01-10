cf37cdf0dce783127cf53438d2cd48aac45e71cf
anaferreira015 <dasilvaferreira.af@hotmail.com>
2020-01-09 21:26:31 -0300

    Set action methods

--- app/Http/Controllers/CommentController.php
+++ app/Http/Controllers/CommentController.php
@@ -1,20 +1,35 @@
-<?php
+<?php 
 
 namespace App\Http\Controllers;
 
+use App\Comment;
+use App\Events\CommentSent;
+use App\Post;
 use Illuminate\Http\Request;
 
 class CommentController extends Controller
 {
-    protected $fillable = ['user_id', 'post_id', 'body'];
-    
-    public function user()
+    public function store(Post $post)
     {
-        return $this->belongsTo(User::class);
+        $this->validate(request(), [
+            'body' => 'required',
+        ]);
+
+        $user = auth()->user();
+
+        $comment = Comment::create([
+            'user_id' => $user->id,
+            'post_id' => $post->id,
+            'body' => request('body'),
+        ]);
+
+        broadcast(new CommentSent($user, $comment))->toOthers();
+
+        return ['status' => 'Message Sent!'];
     }
 
-    public function post()
+    public function index(Post $post)
     {
-        return $this->belongsTo(Post::class);
+        return $post->comments()->with('user')->get();
     }
 }
