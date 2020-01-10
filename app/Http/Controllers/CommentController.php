5d8768b99208a7d74bc8b4686b27efd790436f94
anaferreira015 <dasilvaferreira.af@hotmail.com>
2020-01-09 21:21:17 -0300

    Conclude relationships

--- app/Http/Controllers/CommentController.php
+++ app/Http/Controllers/CommentController.php
@@ -6,5 +6,13 @@ use Illuminate\Http\Request;
 
 class CommentController extends Controller
 {
-    //
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function post()
+    {
+        return $this->belongsTo(Post::class);
+    }
 }
