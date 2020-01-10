7c1d0d26968fc3c2787242fc5a3ff5c08a7ec628
anaferreira015 <dasilvaferreira.af@hotmail.com>
2020-01-09 21:19:17 -0300

    Create user relationship

--- app/User.php
+++ app/User.php
@@ -68,4 +68,9 @@ class User extends Authenticatable
     {
         return $this->hasMany(Post::class);
     }
+
+    public function comments()
+    {
+        return $this->hasMany(Comment::class);
+    }
 }
